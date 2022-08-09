d95936f60aa6a40defe317ab51e782363c68f663
VovaRubakha <97684717+VovaRubakha@users.noreply.github.com>
2022-08-09 16:10:43 +0300

    Add endpoints of login, logout and current. Add authorize middleware

--- models/user.js
+++ models/user.js
@@ -2,19 +2,26 @@ const { Schema, model } = require("mongoose");
 
 const emailRegexp = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
 
-const userSchema = Schema({
+const userSchema = Schema(
+  {
     email: {
-        type: String,
-        match: emailRegexp,
-        required: [true, 'Email is required'],
-        unique: true,
+      type: String,
+      match: emailRegexp,
+      required: [true, "Email is required"],
+      unique: true,
     },
     password: {
-        type: String,
-        minlength: 6,
-        required: [true, 'Password is required'],
+      type: String,
+      minlength: 6,
+      required: [true, "Password is required"],
     },
-}, { versionKey: false, timestamps: true });
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
 const User = model("user", userSchema);
 
