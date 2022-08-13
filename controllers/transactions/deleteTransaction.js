2eb1f432162f67a9a8930a9b33d27e1b696dc87e
Oleg Muraviov <o.muraviov2014@gmail.com>
2022-08-13 18:26:58 +0300

    ctrlWrapper used

--- controllers/transactions/deleteTransaction.js
+++ controllers/transactions/deleteTransaction.js
@@ -2,38 +2,34 @@ const Transaction = require("../../models/transaction");
 const User = require("../../models/schemas/user");
 const { createError } = require("../../helpers");
 
-const deleteTransaction = async (req, res, next) => {
-  try {
-    // ТУТ ТРЕБА ЗРОБИТИ ВАЛІДАЦІЮ
-    const { _id, balance } = req.user;
+const deleteTransaction = async (req, res) => {
+  // ТУТ ТРЕБА ЗРОБИТИ ВАЛІДАЦІЮ
+  const { _id, balance } = req.user;
 
-    const { id } = req.params;
-    const { type, value } = await Transaction.findById({ _id: id });
-    const result = await Transaction.findByIdAndDelete({
-      _id: id,
-    });
-    if (!result) {
-      throw createError(404, "Transaction ID not found");
-    }
-
-    // UPDATE BALANCE - ТРЕБА ВИНЕСТИ В ОКРЕМУ ФУНКЦІЮ
-    let newBalance = +balance;
-    type === "income" ? (newBalance += +value) : (newBalance -= +value);
-
-    const updatedUser = await User.findByIdAndUpdate(
-      { _id },
-      { balance: newBalance },
-      { returnDocument: "after" }
-    );
-    ///////////////////////////////////////////////////////
-
-    res.status(200).json({
-      message: "transaction deleted",
-      balance: updatedUser.balance,
-    });
-  } catch (error) {
-    next(error);
+  const { id } = req.params;
+  const { type, value } = await Transaction.findById({ _id: id });
+  const result = await Transaction.findByIdAndDelete({
+    _id: id,
+  });
+  if (!result) {
+    throw createError(404, "Transaction ID not found");
   }
+
+  // UPDATE BALANCE - ТРЕБА ВИНЕСТИ В ОКРЕМУ ФУНКЦІЮ
+  let newBalance = +balance;
+  type === "income" ? (newBalance += +value) : (newBalance -= +value);
+
+  const updatedUser = await User.findByIdAndUpdate(
+    { _id },
+    { balance: newBalance },
+    { returnDocument: "after" }
+  );
+  ///////////////////////////////////////////////////////
+
+  res.status(200).json({
+    message: "transaction deleted",
+    balance: updatedUser.balance,
+  });
 };
 
 module.exports = deleteTransaction;
